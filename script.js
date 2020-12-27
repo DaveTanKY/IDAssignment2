c1a2afdea0076a8f5696e7d88970bf0bd217834d
DaveTanKY <dave21sg@gmail.com>
2020-12-27 22:46:21 +0800

    Displaying of characters and adding of button

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 
-var charlist = [];
-async function getData(){
+// Retreiving data from API
+var charlist;
+async function getData() {
     const url = 'https://hp-api.herokuapp.com/api/characters'
     const response = await fetch(url);
     const data = await response.json();
-    displayChar(data);
+    charlist = data;
+    displayChar();
 
 }
-
-
 getData();
-
-function displayChar(charlist){
+// Appending data and elements into html
+function displayChar() {
     charlist.forEach(char => {
-        console.log(char.name);
-        $('body').append("<span class='name'>" + char.name + "</span>")
+        $('.char-main-content').append("<div class='char-container'>" + 
+        "<img class='char-img' src='" + char.image + "'>" +
+        "<div class='name'>" + char.name + "</div>"+
+        "<button class='more-info-btn' id='" + char.name + "' onclick='moreInfo(this.id)'>More information</button>" +
+        "</div>");
+        
+
     });
+}
+
+// More info is displayed when button is clicked
+function moreInfo(key){
+    charlist.forEach(char => {
+        if (char.name === key)
+        {
+            alert(char.name);
+        }
+    })
 }
